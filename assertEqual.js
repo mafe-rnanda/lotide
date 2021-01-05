5e0684988a22a5cd73197400501f8055b7948310
Maria Fernanda Chavez <mf.chavezminjarez@gmail.com>
2021-01-05 00:59:53 +0000

    assertion function that compares two values and prints out successful or failure message

---
+++ assertEqual.js
@@ -0,0 +1,18 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅ Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🛑 Assertion Failed: " + actual + " !== " + expected);
+  }
+
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Maria", "MARIA");
+assertEqual(19, 1);
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual("hello", 1);
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(10, 10);
