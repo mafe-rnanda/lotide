a8a7b056eb45c6f97f34cdb1996f7ffab9b0d113
Maria Fernanda Chavez <mf.chavezminjarez@gmail.com>
2021-01-06 21:03:47 +0000

    array comparison by printing message

---
+++ assertArraysEqual.js
@@ -0,0 +1,32 @@
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(`✅ Assertion Passed: ${actual} === expected`);
+//   } else {
+//     console.log(`🛑 Assertion Failed: ${actual} !== expected`);
+//   }
+
+// };
+
+const eqArrays = function(arr1,arr2) {
+    if(arr1.length !== arr2.length) {
+      return false;
+    } else {
+      for(let i = 0; i < arr1.length; i++) {
+        if(arr1[i] !== arr2[i]) {
+          return false
+        } 
+      }
+    }
+    return true;
+  };
+
+  const assertArraysEqual = function(actual, expected) {
+    eqArrays(actual, expected) ? console.log(`✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`)
+  };
+
+
+// TEST CODE
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should PASS
+assertArraysEqual([1, 2, 3], [1, 2, 3, 4]); // =>should FAIL
+assertArraysEqual([1, 2, 3], [3, 2, 3]); // =>should FAIL
+assertArraysEqual([1, 2, 3], [1, 2, '3']); // => should FAIL
