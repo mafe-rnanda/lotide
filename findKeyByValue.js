c4457e421dc94ad1840a65099d828c535a769395
Maria Fernanda Chavez <mf.chavezminjarez@gmail.com>
2021-01-07 21:04:33 +0000

    find key by value

---
+++ findKeyByValue.js
@@ -0,0 +1,29 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    return `✅ Assertion Passed: ${actual} === ${expected}`;
+  } else {
+    return `🛑 Assertion Failed: ${actual} !== ${expected}`;
+  }
+};
+
+//scan the object and return the first key which contains the given value. If no key with that given value is found, then it should return undefined
+const findKeyByValue = function(obj, value) {
+  for (let key in obj) {
+    //console.log(key)
+    const keyValue = obj[key];
+    //console.log(key, value);
+    if (keyValue === value) {
+      return key;
+    }
+  }
+};
+
+//TEST CODE
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+console.log(assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama"));
+console.log(assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined));
