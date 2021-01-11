9e7db601e56bd1e8509f8a7f6bf9dbe928328099
Maria Fernanda Chavez <mf.chavezminjarez@gmail.com>
2021-01-11 22:40:37 +0000

    implementation of mocha and chai functionsstatements

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,25 @@
 const assertEqual = require('../assertEqual');
 const head = require('../head')
 
+const assert = require('chai').assert;
 //////// TEST CODE ////////
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head(["Hello"]), "Bye");
-assertEqual(head([]), "Bye");
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(head(["Hello"]), "Bye");
+// assertEqual(head([]), "Bye");
+
+
+
+describe("Retrieve the first element from the array", () => {
+  it("should return 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("should return '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+
+  it("should return 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello'); 
+  });
+});
