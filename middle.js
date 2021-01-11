cfdbb4cfbb09eca59ca7087985e5b88aa2064f63
Maria Fernanda Chavez <mf.chavezminjarez@gmail.com>
2021-01-11 21:46:12 +0000

    refactoring of code and test files requiring modules instead of copying funcs manually

--- middle.js
+++ middle.js
@@ -1,29 +1,8 @@
-const eqArrays = function (arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  } else {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function (actual, expected) {
-  return eqArrays(actual, expected)
-    ? `✅ Assertion Passed: ${actual} === ${expected}`
-    : `🛑 Assertion Failed: ${actual} !== ${expected}`;
-
-};
-
-// const assertArraysEqual = function(actual, expected) {
-//   return eqArrays(actual, expected) ? `✅ Assertion Passed: ${actual} === ${expected}` : `🛑 Assertion Failed: ${actual} !== ${expected}`
-// };
-
 //take in an array and return the middle-most element(s) of the given array.
-//return an array with only the middle element(s) of the provided array. This means that the length of the returned elements could vary.
+//For arrays with one or two elements, there is no middle. Return an empty array.
+//For arrays with odd number of elements, an array containing a single middle element should be returned.
+//For arrays with an even number of elements, an array containing the two elements in the middle should be returned
+
 const middle = function (array) {
   let middleIndex = [];
   //identify array's length and
@@ -45,23 +24,4 @@ const middle = function (array) {
   return middleIndex; 
 };
 
-//TEST CODE
-//For arrays with one or two elements, there is no middle. Return an empty array.
-middle([1]); // => []
-middle([1, 2]); // => []
-
-//For arrays with odd number of elements, an array containing a single middle element should be returned.
-middle([1, 2, 3]); // => [2]
-middle([1, 2, 3, 4, 5]); // => [3]
-
-//For arrays with an even number of elements, an array containing the two elements in the middle should be returned
-middle([1, 2, 3, 4]); // => [2, 3]
-middle([1, 2, 3, 4, 5, 6]); // => [3, 4]
-
-console.log(assertArraysEqual(middle([]), []));
-console.log(assertArraysEqual(middle([1]), []));
-console.log(assertArraysEqual(middle([1, 2]), []));
-console.log(assertArraysEqual(middle([1, 2, 3]), [2]));
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]));
-console.log(assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]));
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]));
+module.exports = middle;
---
+++ test/middleTest.js
@@ -0,0 +1,11 @@
+const middle = require('../middle');
+const assertArraysEqual = require('../assertArraysEqual');
+
+//////// TEST CODE ////////
+assertArraysEqual(middle([]), []);
+assertArraysEqual(middle([1]), []);
+assertArraysEqual(middle([1, 2]), []);
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
