d446cbd6a89850ba7bd92a4889a655683dc9f666
Maria Fernanda Chavez <mf.chavezminjarez@gmail.com>
2021-01-12 01:09:41 +0000

    export of all lotide functions into object for easy access

---
+++ index.js
@@ -0,0 +1,36 @@
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+
+const assertEqual = require('./assertEqual')
+const assertArraysEqual = require('./assertArraysEqual')
+const assertObjectsEqual = require('./assertObjectsEqual')
+const eqArrays = require('./eqArrays')
+const eqObjects = require('./eqObjects')
+
+module.exports = {
+  head,
+  tail,
+  middle,
+  countLetters,
+  countOnly,
+  findKey,
+  findKeyByValue,
+  letterPositions,
+  map,
+  takeUntil,
+  without,
+  assertEqual,
+  assertArraysEqual,
+  assertObjectsEqual,
+  eqArrays,
+  eqObjects,
+};
